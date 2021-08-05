e7b68032d2be2b1c8dab71d8df98559a88fd78e3
Bad Boy <58816669+Bad-Boy-Codes@users.noreply.github.com>
2021-08-05 13:46:06 +0530

    Update index.js
    d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+const express = require("express"),
+  path = require("path"),
+  Discord = require("discord.js");
+const app = express(),
+  hook = new Discord.WebhookClient(process.env.hookid, process.env.hooktoken);
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+  hook.send(
+    new Discord.MessageEmbed()
+      .setDescription("Someone just started viewing our home page!")
+      .setColor("RED")
+  );
+});
+
+app.get("/text/reverse=:query", (req, res) => {
+  const query = req.params.query;
+  const split = query.split("");
+  const reversed = split.reverse();
+  const response = reversed.join("");
+  hook.send(
+    new Discord.MessageEmbed()
+      .setDescription(`Someone Just Used Reverse Api!\nQuery: \`${query}\``)
+      .setColor("RED")
+  );
+  res.status(200).send({ response });
+});
+
+app.use((req, res) => {
+  res.status(404).send("this route is invalid");
+});
+app.listen(3000, () => console.log("Api online!"));
